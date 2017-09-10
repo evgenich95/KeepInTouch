7f82768bbeac9b0ef29e69c18c81322109443bbf
Anton Ivanov <evgenich95.work@gmail.com>
2017-09-10 18:06:33 +0700

    Create project

---
+++ KeepInTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeepInTouch
+//
+//  Created by Anton Ivanov on 10.09.17.
+//  Copyright © 2017 IAE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
