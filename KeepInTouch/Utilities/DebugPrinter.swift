7a2b60aa088dfc5e60a657da8cab0522a4781060
Anton Ivanov <evgenich95@gmail.com>
2017-12-18 18:37:34 +0700

    Improve printMe() function

--- KeepInTouch/Utilities/DebugPrinter.swift
+++ KeepInTouch/Utilities/DebugPrinter.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-func printMe(filePath: String = #file, functionName: String = #function, with arguments: [Any]? = nil) {
+func printMe(filePath: String = #file, functionName: String = #function, with arguments: Any...) {
     //<FileName>.swift
     let swiftFileName = filePath.components(separatedBy: "/").last ?? ""
     //<FileName>
     let className = swiftFileName.components(separatedBy: ".").first ?? ""
     print("-->\(className).\(functionName)")
-    arguments?.forEach {
+    arguments.forEach {
         print("\t\t\($0)")
     }
 }
